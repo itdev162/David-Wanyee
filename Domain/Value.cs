abbf162bd301fe33300ad4eadeac73d029c26bd4
David Wanyee <dngw@Davids-MacBook-Pro.local>
2020-12-04 14:46:37 -0600

    added

--- Domain/Value.cs
+++ Domain/Value.cs
@@ -5,8 +5,8 @@ namespace Domain
     public class Value
     {
 
-        public int Id {get;set}
+        public int Id {get;set;}
 
-        public string Name{get;set}        
+        public string Name {get;set;}        
     }
 }
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Domain;
+
 
 namespace Persistence
 {
