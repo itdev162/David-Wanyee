8a164f63b908b68f5cfe32c338a9e67eaf23d750
David Wanyee <dngw@Davids-MacBook-Pro.local>
2020-12-05 14:52:47 -0600

    basic stack app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          Blogbox
+        </header>
+      </div>
+  );
+  }
+}
+
+export default App;
